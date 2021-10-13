82d83b26890e08df88256e9d7e1495f4f0cd0a09
Albin Rdz <ealbinu@gmail.com>
2021-10-13 09:29:13 -0500

    fix

--- app/app.js
+++ app/app.js
@@ -1,10 +1,6 @@
 $('.my-paroller').paroller();
 
 
-
-
-
-
 var vm = new Vue({
     el: '#tablas',
     data () {
@@ -16,6 +12,23 @@ var vm = new Vue({
             tabla22Oct: tabla22,
             tablaActividades: tablaAct
         }
+    },
+    methods: {
+        async updateTablas () {
+            const url = 'https://firebasestorage.googleapis.com/v0/b/voces2021-53ac9.appspot.com/o/tablas.json?alt=media'
+            const res = await axios.get(url)
+            const tablas = res.data
+            this.tabla18Oct= tablas.tabla18
+            this.tabla19Oct= tablas.tabla19
+            this.tabla20Oct= tablas.tabla20
+            this.tabla21Oct= tablas.tabla21
+            this.tabla22Oct= tablas.tabla22
+        }
+    },
+    mounted () {
+        this.updateTablas()
+        
+            
     }
 
 })
